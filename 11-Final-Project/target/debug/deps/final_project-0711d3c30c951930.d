80528c954573ec068abdcb429563fb43a9eb91a6
Tuong <zrus@192.168.2.5>
2024-03-13 04:06:43 +0700

    chore: remove .lock and target folder

--- 11-Final-Project/target/debug/deps/final_project-0711d3c30c951930.d
+++
@@ -1,8 +0,0 @@
-/Users/zrus/workspace/learning/Rust-bootcamp-2024/11-Final-Project/target/debug/deps/libfinal_project-0711d3c30c951930.rmeta: src/lib.rs src/energy.rs
-
-/Users/zrus/workspace/learning/Rust-bootcamp-2024/11-Final-Project/target/debug/deps/libfinal_project-0711d3c30c951930.rlib: src/lib.rs src/energy.rs
-
-/Users/zrus/workspace/learning/Rust-bootcamp-2024/11-Final-Project/target/debug/deps/final_project-0711d3c30c951930.d: src/lib.rs src/energy.rs
-
-src/lib.rs:
-src/energy.rs:
--- 11-Final-Project/target/debug/deps/final_project-7f06f45a8d8cdde3.d
+++
@@ -1,6 +0,0 @@
-/Users/zrus/workspace/learning/Rust-bootcamp-2024/11-Final-Project/target/debug/deps/libfinal_project-7f06f45a8d8cdde3.rmeta: src/lib.rs src/energy.rs
-
-/Users/zrus/workspace/learning/Rust-bootcamp-2024/11-Final-Project/target/debug/deps/final_project-7f06f45a8d8cdde3.d: src/lib.rs src/energy.rs
-
-src/lib.rs:
-src/energy.rs:
